7ea99eb77f3adfabb86c06a2963f4c9e5c7181a8
roger <rmclanton@email.wm.edu>
2020-09-25 17:39:54 -0400

    Edited settings.ini and  created 09_utils
    Editing setttings.ini will need to be undone to resest it for the WM_SEMERU/SecureReqNet repository
    09_utils currently holds the data from read_data

--- securereqnet/_nbdev.py
+++ securereqnet/_nbdev.py
@@ -2,12 +2,15 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"say_hello": "00_core.ipynb",
+         "Dynamic_Dataset": "09_utils.ipynb",
+         "Processing_Dataset": "09_utils.ipynb"}
 
-modules = []
+modules = ["core.py",
+           "utils.py"]
 
 doc_url = "https://rmclanton.github.io/securereqnet/"
 
-git_url = "https://github.com/rmclanton/SecureReqNet"
+git_url = "https://github.com/rmclanton/securereqnet/tree/master/"
 
 def custom_doc_links(name): return None

08a4436cb8910c9ae7806ef353bd622f85751f0f
roger <rmclanton@email.wm.edu>
2020-10-15 14:38:57 -0400

    Added test input TFrecords to data and 2 text file representations

--- recordTest.py
+++ recordTest.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
-raw_dataset = tf.data.TFRecordDataset("nbs/testdata.tfrecord")
+import sys
+
+#input the path to the tfrecord you want to examine
+raw_dataset = tf.data.TFRecordDataset(sys.argv[1])
 for raw_record in raw_dataset.take(10):
     example = tf.train.Example()
     example.ParseFromString(raw_record.numpy())
